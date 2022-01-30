b4d1d96c72e1a6cc0978fa91a60668e3c3fe61fb
Duane Goodner <dmgoodner@gmail.com>
2022-01-30 11:24:54 -0700

    commit before change to non-templated main

--- safe_repkg/enums.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum, auto
-
-
-class ReqSource(Enum):
-    TEXT = auto()
-    CONDA_YML = auto()
-    PROJ_TOML = auto()
-    SETUP_CFG = auto()
-    NONE = auto()
--- safe_repkg/safe_pkg_builder.py
+++ safe_repkg/safe_pkg_builder.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 import string
 from shutil import copytree, ignore_patterns, copy2
 
@@ -6,75 +7,75 @@ from shutil import copytree, ignore_patterns, copy2
 class HpkgBuilder:
     _ignore_types = ['*.git', '*.gitignore', '*.idea', '*__pycache__']
 
-    def __init__(self, stand_path: str, safe_path: str):
-        self._stand_path = stand_path
-        self._safe_path = safe_path
-        self._pkg_name = os.path.basename(stand_path)
+    def __init__(self, orig_path: Path, h_path: Path):
+        self._orig_path = orig_path
+        self._h_path = h_path
+        self._pkg_name = orig_path.name
 
     def copy_package(self):
-        try:
-            copy_location = copytree(
-                self._stand_path, self._safe_path, ignore=ignore_patterns(
-                    *self._ignore_types))
-            components_sub_dir = copytree(os.getcwd() +
-                                          '/install_components/hpkg_components',
-                                          copy_location + '/hpkg_components')
 
-            copy2(os.getcwd() + '/install_components' + '/hpkg_driver.py',
-                  self._safe_path + '/hpkg_' + os.path.basename(
-                      self._stand_path) + '.py')
+        try:
+            copytree(
+                self._orig_path, self._h_path, ignore=ignore_patterns(
+                    *self._ignore_types))
+
+            copytree(Path(__file__).parent.absolute() /
+                     'install_components' /
+                     'hpkg_components',
+                     self._h_path /
+                     'hpkg_components')
+
+            copy2(Path(__file__).parent.absolute() / 'install_components' /
+                  'hpkg_driver.py',
+                  self._h_path / ('hpkg_' + str(self._pkg_name) + '.py'))
 
         except OSError:
             print("Error when attempting to copy")
-            return
-
-        return copy_location
+            exit(1)
 
     def move_orig_safe_main(self):
-        os.rename(self._safe_path + '/' + os.path.basename(self._stand_path) +
-                  '/__main__.py',
-                  self._safe_path + '/' + os.path.basename(self._stand_path) +
-                  '/old_main.py')
+        (self._h_path / self._pkg_name / '__main__.py').rename(
+            self._h_path / self._pkg_name / 'old_main.py')
 
     def write_pkg_name(self):
-        with open('package_name.py.template', 'r') as f:
+
+        # TODO fix to maek compatbile with Path object
+        with (Path(__file__).parent.absolute() /
+              'package_name.py.template').open(mode='r') as f:
             template_text = f.read()
 
         template = string.Template(template_text)
 
         substitutions = {
-            'hpkg_path': self._safe_path
+            'hpkg_path': self._h_path
         }
 
         result = template.substitute(substitutions)
 
-        with open(self._safe_path + '/hpkg_components/hpkg_header.py',
-                  'w') as f:
+        with Path(self._h_path / 'hpkg_components' / 'hpkg_header.py')\
+                .open(mode='w') as f:
             f.write(result)
-
-    # TODO use pkgutils to properly access .template file
 
     def rebuild_safe_main(self):
-        with open('safe_main.py.template', 'r') as f:
+        with (Path(__file__).parent.absolute() /
+              'safe_main.py.template').open(mode='r') as f:
             template_text = f.read()
 
         template = string.Template(template_text)
 
         substitutions = {
-            'cur_package_name': os.path.basename(self._stand_path)
+            'cur_package_name': os.path.basename(self._orig_path)
         }
 
         result = template.substitute(substitutions)
 
-        with open('new_safe_main.py', 'w') as f:
+        with (self._h_path / self._pkg_name / '__main__.py')\
+                .open(mode='w') as f:
             f.write(result)
 
-        os.rename('new_safe_main.py', self._safe_path + '/' +
-                  os.path.basename(self._stand_path) + '/__main__.py')
 
-
-test_builder = HpkgBuilder('/Users/duane/dproj/xiangqigame',
-                           '/Users/duane/dproj/safe_repkg/xiangqigame')
+test_builder = HpkgBuilder(Path('/Users/duane/dproj/xiangqigame'),
+                           Path('/Users/duane/dproj/safe_repkg/xiangqigame'))
 test_builder.copy_package()
 test_builder.move_orig_safe_main()
 test_builder.write_pkg_name()
