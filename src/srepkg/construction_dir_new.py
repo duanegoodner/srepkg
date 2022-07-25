2032b6cfbddfc3095ebae7db25d2e802897460ff
Duane Goodner <dmgoodner@gmail.com>
2022-07-25 17:20:29 -0600

    add orig_pkg_src_summary and wheel_data attributes to ConstructionDir. Populate these during finalize()

--- src/srepkg/construction_dir_new.py
+++ src/srepkg/construction_dir_new.py
@@ -8,7 +8,9 @@ import tempfile
 import uuid
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Callable, List
+from typing import Any, Callable, List, Union
+
+import wheel_inspect
 
 import srepkg.error_handling.custom_exceptions as ce
 import srepkg.utils.dist_archive_file_tools as cft
@@ -27,38 +29,58 @@ class DistInfo:
 
 
 @dataclass
-class OrigSrcSummary:
+class OrigPkgSrcSummary:
     pkg_name: str
     pkg_version: str
     dists: List[DistInfo]
 
     @property
     def has_wheel(self):
-        return any([isinstance(dist.dist_obj, pkginfo.Wheel) for dist in
+        return any([type(dist.dist_obj) == pkginfo.Wheel for dist in
                     self.dists])
 
     @property
+    def wheel_path(self):
+        if self.has_wheel:
+            return [dist.path for dist in self.dists if type(dist.dist_obj) ==
+                    pkginfo.Wheel][0]
+
+    @property
     def has_sdist(self):
-        return any([isinstance(dist.dist_obj, pkginfo.SDist) for dist in
+        return any([type(dist.dist_obj) == pkginfo.SDist for dist in
                     self.dists])
 
 
-class ConstructionDir(re_int.SettleableSrepkgDirInterface,
-                      osp_int.WritableSrepkgDirInterface):
+class WriteableSettleableSrepkgDirInterface(re_int.SettleableSrepkgDirInterface,
+                                            osp_int.WritableSrepkgDirInterface):
+    @abc.abstractmethod
+    def settle(self):
+        pass
+
+    @abc.abstractmethod
+    def finalize(self):
+        pass
+
+
+class ConstructionDir(WriteableSettleableSrepkgDirInterface):
     @abc.abstractmethod
     def __init__(
             self,
             construction_dir_arg: Path,
             srepkg_name_arg: str = None,
             supported_dist_types:
-            tuple[Callable[..., pkginfo.Distribution]] = DEFAULT_DIST_CLASSES):
+            tuple[Callable[..., pkginfo.Distribution]] = DEFAULT_DIST_CLASSES,
+            orig_pkg_src_summary: Union[OrigPkgSrcSummary, type(None)] = None,
+            wheel_data: Union[dict[str, any], None] = None):
         self._root = construction_dir_arg
         self._srepkg_root = construction_dir_arg / uuid.uuid4().hex
-        self._srepkg_root.mkdir()
         self._srepkg_inner = self._srepkg_root / uuid.uuid4().hex
+        self._srepkg_root.mkdir()
         self._srepkg_inner.mkdir()
         self._custom_srepkg_name = srepkg_name_arg
         self._supported_dist_types = supported_dist_types
+        self._orig_pkg_src_summary = orig_pkg_src_summary
+        self._wheel_data = wheel_data
 
     @property
     def root(self):
@@ -88,10 +110,27 @@ class ConstructionDir(re_int.SettleableSrepkgDirInterface,
     def supported_dist_types(self):
         return self._supported_dist_types
 
+    @property
+    def orig_pkg_src_summary(self):
+        return self._orig_pkg_src_summary
+
+    @orig_pkg_src_summary.setter
+    def orig_pkg_src_summary(self, construction_dir_summary: OrigPkgSrcSummary):
+        self._orig_pkg_src_summary = construction_dir_summary
+    
+    @property
+    def wheel_data(self):
+        return self._wheel_data
+    
+    @wheel_data.setter
+    def wheel_data(self, inspect_wheel_result: dict[str, Any]):
+        self._wheel_data = inspect_wheel_result
+
     def _rename_sub_dirs(self, srepkg_root_new: str, srepkg_inner_new: str):
         new_srepkg_root_path = Path(self._root / srepkg_root_new)
         new_srepkg_root_path.mkdir()
-        new_srepkg_inner_path = Path(self._root / srepkg_root_new / srepkg_inner_new)
+        new_srepkg_inner_path = Path(
+            self._root / srepkg_root_new / srepkg_inner_new)
         new_srepkg_inner_path.mkdir()
         for item in self._srepkg_inner.iterdir():
             shutil.move(item, new_srepkg_inner_path)
@@ -115,15 +154,21 @@ class ConstructionDir(re_int.SettleableSrepkgDirInterface,
 
     def finalize(self):
         # construction_dir_summary = ConstructionDirReviewer(self).get_summary()
-        construction_dir_summary = ConstructionDirReviewer(self) \
+        prelim_orig_pkg_src_summary = ConstructionDirReviewer(self) \
             .get_existing_dists_summary()
 
-        if (not construction_dir_summary.has_wheel) and \
-                construction_dir_summary.has_sdist:
-            SdistToWheelConverter(self, construction_dir_summary).build_wheel()
+        if (not prelim_orig_pkg_src_summary.has_wheel) and \
+                prelim_orig_pkg_src_summary.has_sdist:
+            SdistToWheelConverter(self, prelim_orig_pkg_src_summary).build_wheel()
+
+        self._orig_pkg_src_summary = ConstructionDirReviewer(self)\
+            .get_existing_dists_summary()
+
+        self._wheel_data = wheel_inspect.inspect_wheel(
+            self._orig_pkg_src_summary.wheel_path)
 
         self._update_sub_dir_names(
-            discovered_pkg_name=construction_dir_summary.pkg_name)
+            discovered_pkg_name=prelim_orig_pkg_src_summary.pkg_name)
 
 
 class CustomConstructionDir(ConstructionDir):
@@ -149,7 +194,7 @@ class SdistToWheelConverter:
     def __init__(
             self,
             construction_dir: ConstructionDir,
-            construction_dir_summary: OrigSrcSummary):
+            construction_dir_summary: OrigPkgSrcSummary):
         self._construction_dir = construction_dir
         self._construction_dir_summary = construction_dir_summary
         self._compressed_file_extractor = cft.CompressedFileExtractor()
@@ -226,12 +271,12 @@ class ConstructionDirReviewer:
 
         unique_pkg = unique_pkgs[0]
 
-        return OrigSrcSummary(
+        return OrigPkgSrcSummary(
             pkg_name=unique_pkg[0],
             pkg_version=unique_pkg[1],
             dists=existing_dists)
 
-    def get_existing_dists_summary(self) -> OrigSrcSummary:
+    def get_existing_dists_summary(self) -> OrigPkgSrcSummary:
 
         existing_dists = self._get_existing_dists()
         return self._validate_existing_dists(existing_dists)
