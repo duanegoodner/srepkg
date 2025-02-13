f87ca16a585105132ca124fffe7aef7ffda8cac4
Duane Goodner <dmgoodner@gmail.com>
2025-02-12 22:54:42 -0700

    Use WheelEntryPointsModifier to handle dashes in console script names (#17)
    * black_as_blacksrepkg working with pyproject.toml
    
    * black_as_blacksrepkg working with pyproject.toml
    
    * replcae dash w/ underscore in entrypoint bin filenames
    
    * add entry_point_name_modifier
    
    * replace '-' in console script names with '_'; can now handle numpy 2
    
    * clean up dev files

---
+++ prototypes/black_as_blacksrepkg_orig/cmd_classes.py
@@ -0,0 +1,12 @@
+from setuptools.command.bdist_wheel import bdist_wheel
+
+
+class BdistWheelAbi3(bdist_wheel):
+    def get_tag(self):
+        python, abi, plat = super().get_tag()
+
+        if python.startswith("cp"):
+            # on CPython, our wheels are abi3 and compatible back to 3.6
+            return "cp36", "abi3", plat
+
+        return python, abi, plat
--- test/troubleshooter_srepkg_no_mock.py
+++ test/troubleshooter_srepkg_no_mock.py
@@ -14,7 +14,7 @@ def test_srepkg(mocker, sample_pkgs):
 
 
 def run_repkg():
-    main(["numpy", "-r", "2.0.0"])
+    main(["numpy", "-r", "2.2.2"])
 
 
 if __name__ == "__main__":
