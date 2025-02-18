6d9dd230ba06b02d47356bc8806074fead6e95c8
Duane Goodner <dmgoodner@gmail.com>
2025-02-18 13:49:45 -0700

    fix import shared_fixtures import path to work with local debug

--- test/troubleshooter_srepkg_no_mock.py
+++ test/troubleshooter_srepkg_no_mock.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import srepkg.repackager as rep
 from srepkg.srepkg import main
-from test.shared_fixtures import sample_pkgs
+from shared_fixtures import sample_pkgs
 
 
 def test_srepkg(mocker, sample_pkgs):
